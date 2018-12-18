d6e615085ff65c032aadebfd18f7d1afadefbd52
Raniro Coelho <errecme@gmail.com>
2018-12-18 18:17:18 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/App.sass';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <h1>MY STOCK APP</h1>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from "react-router-dom";
+import Route from "react-router-dom/Route";
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.sass';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <Router>
+    <Route exact path="/" component={App} />
+  </Router>,
+  document.getElementById("root"));
+
+serviceWorker.unregister();
