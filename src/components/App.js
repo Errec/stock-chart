d623213a56a94835327319afa7d7580a97f2e7f7
Raniro Coelho <errecme@gmail.com>
2018-12-18 18:20:30 +0000

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import '../style/App.sass';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-          <h1>MY STOCK APP</h1>
+          <Header></Header>
         </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/Header.sass';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className = "Header" >
+                <h1> Stock View </h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
