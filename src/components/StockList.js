ff450f9e75d232e2e3b09200e24d2a442cc6617d
Raniro Coelho <errecme@gmail.com>
2018-12-25 13:29:41 +0000

    Add Prototype req

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -9,51 +9,60 @@ import {
 import { fetchGainers} from '../actions/gainersActions';
 import '../style/StockList.sass';
 import StockItem from './StockItem';
+import PropTypes from 'prop-types';
 
 class StockList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            isLoaded: false,
-            error: 0
-        };
-    }
 
-    componentWillMount() {
-      this.props.fetchGainers();
-    }
 
-    render() {
-        this.state.isLoaded = this.props.data.isLoaded;
-        this.state.error = this.props.data.gainers.length;
-        if (!this.state.isLoaded) {
-          return <div>Loading...</div>;
-        } else if (this.state.error) {
-          return <Router>
-              <div className="StockList">
-                <ul>
-                  {this.props.data.gainers.map(stock => (
-                    <Link key={stock.symbol} to={stock.symbol}>
-                      <li>
-                        {stock.companyName} <span>{stock.high}</span>
-                      </li>
-                      <Switch>
-                      <Route
-                        exact
-                        path={"/" + stock.symbol}
-                        component={StockItem}
-                      />
-                      </Switch>
-                    </Link>
-                  ))}
-                </ul>
-              </div>
-            </Router>;
-        } else if (!this.state.error) {
-          return <p>Error Loading Data</p>
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+        isLoaded: false,
+        error: 0
+    };
+  }
+
+  componentWillMount() {
+    this.props.fetchGainers();
+  };
+
+
+  render() {
+      this.state.isLoaded = this.props.data.isLoaded;
+      this.state.error = this.props.data.gainers.length;
+      if (!this.state.isLoaded) {
+        return <div>Loading...</div>;
+      } else if (this.state.error) {
+        return <Router>
+            <div className="StockList">
+              <ul>
+                {this.props.data.gainers.map(stock => (
+                  <Link key={stock.symbol} to={stock.symbol}>
+                    <li>
+                      {stock.companyName} <span>{stock.high}</span>
+                    </li>
+                    <Switch>
+                    <Route
+                      exact
+                      path={"/" + stock.symbol}
+                      component={StockItem}
+                    />
+                    </Switch>
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          </Router>;
+      } else if (!this.state.error) {
+        return <p>Error Loading Data</p>
+      }
+  }
 }
+
+StockList.propTypes = {
+  fetchGainers: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
   data : state.gainers.gainersData
